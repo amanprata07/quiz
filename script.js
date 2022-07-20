5a14e8f1b6c124ce7604f208a5fc33fa6d862ae8
amanprata07 <108935523+amanprata07@users.noreply.github.com>
2022-07-20 13:29:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Who is the ceo of Birlasoft?",
+    answer: "Dharmendra Kapoor",
+    options: [
+      "Dharmendra Sharma",
+      "Dharmendra Singh",
+      "Dharmendra Kapoor",
+      "Dharmendra Kumar"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Birlasoft is Founded In?",
+    answer: "1995",
+    options: [
+      "1995",
+      "1996",
+      "1994",
+      "1997"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+];
